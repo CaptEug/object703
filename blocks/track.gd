49a2fd4e2aed8255e65306337d0775877b77cc98
eug <capteug324@gmail.com>
2025-05-25 15:24:29 +0800

    Initialize

---
+++ blocks/track.gd
@@ -0,0 +1,18 @@
+extends Block
+
+const HITPOINT:int = 100
+const WEIGHT:int = 1
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func forward():
+	pass
+
+func backward():
+	pass
