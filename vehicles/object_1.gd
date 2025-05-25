ae52ef366e5a966bab1b84ea2226d170fcdea354
YZMYT <1563400181@qq.com>
2025-05-25 17:55:19 +0800

    forward_to_object

--- vehicles/object_1.gd
+++ vehicles/object_1.gd
@@ -11,4 +11,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_pressed("FORWARD"):  # Typically W key
+		$Track_left.state = 'forward'
+		$Track_right.state = 'forward'
+	elif Input.is_action_pressed("BACKWARD"):  # Typically S key
+		$Track_left.state = 'backward'
+		$Track_right.state = 'backward'
+	else:
+		$Track_left.state = ''
+		$Track_right.state = ''
 	pass
