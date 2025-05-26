276d524f431414d0e9797ec5dc788b418fb74e8f
eug <capteug324@gmail.com>
2025-05-26 20:38:53 +0800

    new joints

--- vehicles/vehicle.gd
+++ vehicles/vehicle.gd
@@ -1,6 +1,7 @@
 class_name Vehicle
 extends Node2D
 
+const GRID_SIZE:int = 16
 var move_state:String
 var total_power:float
 var total_weight:int
@@ -23,7 +24,7 @@ func _process(delta):
 
 func connect_blocks():
 	for block in get_tree().get_nodes_in_group('blocks'):
-		var snapped_pos = snap_block_to_grid(block, 16, true)
+		var snapped_pos = snap_block_to_grid(block)
 		grid[snapped_pos] = block
 		connect_adjacent_blocks(snapped_pos, block)
 
@@ -44,32 +45,36 @@ func update_tracks_state(delta):
 	else:
 		move_state = 'idle'
 
-func snap_block_to_grid(block: Block, grid_size: int = 16, align_to_center: bool = true) -> Vector2i:
+func snap_block_to_grid(block:Block) -> Vector2i:
 	var world_pos = block.global_position
 	var snapped_pos = Vector2(
-		floor(world_pos.x / grid_size),
-		floor(world_pos.y / grid_size)
+		floor(world_pos.x / GRID_SIZE),
+		floor(world_pos.y / GRID_SIZE)
 	)
-	if align_to_center:
-		block.global_position = snapped_pos * grid_size + Vector2(grid_size / 2, grid_size / 2)
-	else:
-		block.global_position = snapped_pos * grid_size
+	block.global_position = snapped_pos * GRID_SIZE + block.size/2 * GRID_SIZE
 	return snapped_pos  # useful for tracking in a grid dictionary
 
-func connect_adjacent_blocks(pos: Vector2i, block: Block):
+func connect_adjacent_blocks(pos:Vector2i, block:Block):
 	var directions = [Vector2i.LEFT, Vector2i.RIGHT, Vector2i.UP, Vector2i.DOWN]
 	for dir in directions:
 		var neighbor_pos = pos + dir
 		if grid.has(neighbor_pos):
 			var neighbor = grid[neighbor_pos]
-			connect_with_joint(block, neighbor)
+			connect_with_joint(block, neighbor, dir)
 
-func connect_with_joint(a: Block, b: Block):
+func connect_with_joint(a:Block, b:Block, dir):
 	var joint = PinJoint2D.new()
 	joint.node_a = a.get_path()
 	joint.node_b = b.get_path()
 	# Place joint in the middle of the two blocks
-	joint.position = (a.global_position + b.global_position) / 2.0
+	if dir == Vector2i.LEFT:
+		joint.position.x = - a.size.x * GRID_SIZE / 2.0
+	if dir == Vector2i.RIGHT:
+		joint.position.x = a.size.x * GRID_SIZE / 2.0
+	if dir == Vector2i.UP:
+		joint.position.y = - a.size.y * GRID_SIZE / 2.0
+	if dir == Vector2i.DOWN:
+		joint.position.y = a.size.y * GRID_SIZE / 2.0
 	joint.disable_collision = false
-	add_child(joint)
+	a.add_child(joint)
 	
