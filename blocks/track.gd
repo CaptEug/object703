811a4f9f6fc1eb175c126ad91335579eeb33614d
YZMYT <1563400181@qq.com>
2025-05-27 18:58:47 +0800

    2025/5/27 18:59

--- blocks/track.gd
+++ blocks/track.gd
@@ -2,20 +2,20 @@ extends Block
 
 const HITPOINT:int = 100
 const WEIGHT:int = 1
+const FRACTION = 5.0
 var size:= Vector2(1, 1)
-var state:String
-var force:int
-var state_force: Array = ['', 0]
-var fraction = 0.1 
+var state_force: Array = ['idle', 0.0]
 var force_direction := Vector2.ZERO
+var max_force = 500
 
 func get_weight() -> float:
 	return WEIGHT
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	linear_damp = FRACTION
 	add_to_group('tracks')
-	set_state_force('idle', 0)
-	_set_linear_damp()
+	set_state_force('idle', 0.0)
+	print(state_force)
 	queue_redraw()
 	pass # Replace with function body.
 
@@ -26,12 +26,9 @@ func _process(delta):
 
 func _physics_process(delta):
 	track_move(delta)
-	_set_linear_damp()
 		
-func set_state_force(new_state: String, new_force: int):
-	state = new_state
-	force = new_force
-	state_force = [new_state, new_force]
+func set_state_force(new_state: String, force_value: float):
+	state_force = [new_state, clamp(force_value, -max_force, max_force)]
 
 func _on_received_state_force_signal(state_force_signal):
 	if state_force_signal is Array and state_force_signal.size() >= 2:
@@ -48,13 +45,5 @@ func track_move(delta):
 		force_direction = Vector2.DOWN
 
 
-func _set_linear_damp():
-	if state_force[0] == 'idle':
-		linear_damp = fraction * 60
-	else:
-		linear_damp = fraction
-
-
 func _draw():
-	draw_line(Vector2.ZERO, force_direction * force * 10, Color.RED, 2)
-	
+	draw_line(Vector2.ZERO, force_direction * state_force[1] * 10, Color.RED, 2)
