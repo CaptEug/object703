5bdad21e7ecb6068b88fa8a0e8f3dd025f104726
eug <capteug324@gmail.com>
2025-06-03 13:00:00 +0800

    minor changes

--- blocks/weapons/ammorack.gd
+++ blocks/weapons/ammorack.gd
@@ -1,18 +1,19 @@
 extends Block
 
 const HITPOINT:int = 100
-const WEIGHT:float = 200
+const WEIGHT:float = 1.0
 const BLOCK_NAME:String = 'ammo rack'
 const SIZE:= Vector2(1, 1)
 var ammo_capacity:float = 50.0
 
 func init():
-	mass = WEIGHT
 	current_hp = HITPOINT
+	weight = WEIGHT
+	block_name = BLOCK_NAME
+	size = SIZE
+	linear_damp = 5.0
+	angular_damp = 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
