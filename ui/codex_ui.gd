de1f5cfc937a1ae206926ffe5d4b13fb0ce2e60b
eug <capteug324@gmail.com>
2025-06-25 17:49:43 +0800

    block codex

--- ui/codex_ui.gd
+++ ui/codex_ui.gd
@@ -4,6 +4,14 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+
+	prepare_data()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func prepare_data():
 	var root = tree.create_item()
 	root.set_text(0, "Blocks")
 	
@@ -12,7 +20,24 @@ func _ready():
 	weapon.set_text(0, "Weapon")
 	var power = tree.create_item(root)
 	power.set_text(0, "Power")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	
+	for scene in get_scenes_from_folder("res://blocks/weapon/"):
+		var block = scene.instantiate()
+		var item = tree.create_item(weapon)
+		item.set_text(0, block.BLOCK_NAME)
+	
+	for scene in get_scenes_from_folder("res://blocks/power/"):
+		var block = scene.instantiate()
+		var item = tree.create_item(power)
+		item.set_text(0, block.BLOCK_NAME)
+		item.set_icon(0, preload("res://assets/engine_icon.png"))
+		
+func get_scenes_from_folder(folder_path: String) -> Array:
+	var scenes = []
+	var dir = DirAccess.open(folder_path)
+	if dir:
+		for file in dir.get_files():
+			if file.ends_with(".tscn"):
+				var path = folder_path + "/" + file
+				scenes.append(load(path))
+	return scenes
