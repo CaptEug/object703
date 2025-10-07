8db4ffe8b43b22058e1d7ed52cbc0db897dce8f4
eug <capteug324@gmail.com>
2025-10-07 14:14:41 +0800

    clock

---
+++ blocks/auxiliary/large_cargo.gd
@@ -0,0 +1,19 @@
+extends Cargo
+
+const HITPOINT:float = 2000
+const WEIGHT:float = 500
+const BLOCK_NAME:String = 'large cargo'
+const SIZE:= Vector2(2, 2)
+const MAX_LOAD:float = 8000
+const TYPE:= 'Auxiliary'
+const ACCEPT:= ["ALL"]
+
+
+func _init():
+	max_hp = HITPOINT
+	current_hp = HITPOINT
+	weight = WEIGHT
+	block_name = BLOCK_NAME
+	accept = ACCEPT
+	size = SIZE
+	type = TYPE
---
+++ scenes/day&night.gd
@@ -0,0 +1,23 @@
+extends CanvasModulate
+
+@export var cycle_duration := 20.0
+@export var day_color := Color(1, 1, 1)
+@export var night_color := Color(0.1, 0.1, 0.25)
+var time := 0.0
+@onready var HUD := get_tree().current_scene.find_child("CanvasLayer").find_child("Hud")
+
+func _process(delta):
+	time = fmod(time + delta, cycle_duration)
+	var t = time / cycle_duration
+	 # Shift the sine phase so 0.25 = sunrise, 0.5 = noon, 0.75 = sunset
+	var brightness = clamp((sin(t * TAU - TAU/4) + 1.0) / 2.0, 0.0, 1.0)
+	color = day_color.lerp(night_color, 1.0 - brightness)
+	
+	if HUD:
+		HUD.time = get_clock_string()
+
+func get_clock_string() -> String:
+	var total_minutes = (time / cycle_duration) * 24.0 * 60.0
+	var hour = int(total_minutes / 60.0) % 24
+	var minute = int(total_minutes) % 60
+	return "%02d:%02d" % [hour, minute]
