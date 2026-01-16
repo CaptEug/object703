62cd54f9410be5c38a7de44208ddb5bc606eb703
eug <capteug324@gmail.com>
2026-01-16 17:20:23 +0800

    engine fuel consumption

--- blocks/mobility/power_pack.gd
+++ blocks/mobility/power_pack.gd
@@ -4,6 +4,7 @@ extends Block
 # 发动机属性
 var power: float = 0
 var max_power: float
+var fuel_consumption:float
 var power_change_rate: float
 var target_power: float
 var state = {"move": false, "rotate": false}
