a1eb0f5839c23f982a5407efa11e205e54e95f66
eug <capteug324@gmail.com>
2026-01-19 14:05:22 +0800

    picking pickup

--- items/item_pickup.gd
+++ items/item_pickup.gd
@@ -1,20 +1,36 @@
 class_name Pickup
 extends Area2D
 
-@export var attract_speed := 600.0
+@export var attract_speed := 200.0
 @export var collect_distance := 16.0
 
 var item_id:String
 var amount:int
 var target: Cargo = null
 var attracting := false
-
+var velocity:Vector2
+var bursting := true
+var burst_speed := 200
+var burst_duration := 1.0
+var burst_time := 0.0
+var drag := 10
 
 func _ready():
-	pass # Replace with function body.
-
+	$Sprite2D.texture = ItemDB.get_item(item_id)["icon"]
+	burst()
 
 func _physics_process(delta):
+	if bursting:
+		burst_time += delta
+		global_position += velocity * delta
+
+		# smooth slowdown
+		velocity = velocity.move_toward(Vector2.ZERO, drag * burst_speed * delta)
+
+		if burst_time >= burst_duration:
+			bursting = false
+		return
+	
 	if attracting and target:
 		var dir = (target.global_position - global_position)
 		var dist = dir.length()
@@ -24,18 +40,26 @@ func _physics_process(delta):
 			queue_free()
 			return
 		
-		global_position += dir.normalized() * attract_speed * delta
+		global_position += dir.normalized() * attract_speed * clamp(64/dist, 0.2, 1.0) * delta
 
+
+func burst():
+	velocity = Vector2.RIGHT.rotated(randf() * TAU) * burst_speed
 
 
 func _on_area_entered(area):
 	var cargo := area.get_parent() as Cargo
+	if not cargo:
+		return
 	if cargo.can_accept_item(item_id):
-		target = area
+		target = cargo
 		attracting = true
 
 
 func _on_area_exited(area):
-	if area == target:
+	var cargo := area.get_parent() as Cargo
+	if not cargo:
+		return
+	if cargo == target:
 		target = null
 		attracting = false
--- tilemaps/wall_layer.gd
+++ tilemaps/wall_layer.gd
@@ -2,11 +2,11 @@ class_name WallLayer
 extends TileMapLayer
 
 var layerdata:Dictionary[Vector2i, Dictionary]
-
+var item_pickup:PackedScene = preload("res://items/item_pickup.tscn")
+var map:Node2D
 
 func _ready():
-	pass # Replace with function body.
-
+	map = get_parent()
 
 
 func _process(delta):
@@ -60,8 +60,25 @@ func damage_tile(cell:Vector2i, amount:int):
 		get_tree().current_scene.add_child(shard)
 
 func destroy_tile(cell:Vector2i):
-	var tile_data = get_cell_tile_data(cell)
+	#shard particle
+	var particle_path = get_cell_tile_data(cell).get_custom_data("particle_path")
+	var shard = load(particle_path).instantiate()
+	shard.position = map_to_local(cell)
+	shard.emitting = true
+	get_tree().current_scene.add_child(shard)
+	
+	#pickup
+	spawn_pickup(cell)
+	
 	erase_cell(cell)
 	layerdata.erase(cell)
 	BetterTerrain.update_terrain_cell(self, cell, true)
-	
+
+
+func spawn_pickup(cell:Vector2i):
+	var item_id = layerdata[cell]["matter"]
+	var pickup = item_pickup.instantiate() as Pickup
+	pickup.item_id = item_id
+	pickup.amount = 1
+	pickup.position = map_to_local(cell)
+	map.add_child(pickup)
