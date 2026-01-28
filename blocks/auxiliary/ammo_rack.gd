21694c72d177799b3670dfc12f0af95fe35eec5e
eug <capteug324@gmail.com>
2026-01-28 18:26:16 +0800

    remove ammorack

--- blocks/auxiliary/ammo_rack.gd
+++ blocks/auxiliary/ammo_rack.gd
@@ -1,12 +1,25 @@
-class_name Ammorack
 extends Cargo
 
+const HITPOINT:float = 600
+const WEIGHT:float = 500
+const BLOCK_NAME:String = 'ammo rack'
+const SIZE:= Vector2(1, 1)
+const TYPE:= "Firepower"
 const ACCEPT:= ["ammo"]
-var ammo_storage :float
+
 var explosion_area:Area2D
 var explosion_shape:CollisionShape2D
 var exploded:bool = false
 var explosion_particle = preload("res://assets/particles/explosion.tscn")
+var description := ""
+
+func _init():
+	max_hp = HITPOINT
+	current_hp = HITPOINT
+	weight = WEIGHT
+	block_name = BLOCK_NAME
+	size = SIZE
+	type = TYPE
 
 func _ready():
 	super._ready()
@@ -17,8 +30,7 @@ func _ready():
 func _process(_delta):
 	super._process(_delta)
 	#update explosion radius
-	explosion_shape.shape.radius = ammo_storage/2
-
+	explosion_shape.shape.radius = 16
 
 func destroy():
 	explode()
@@ -33,9 +45,8 @@ func explode():
 	explosion.emitting = true
 	get_tree().current_scene.add_child(explosion)
 	exploded = true
-	var max_explosive_damage = ammo_storage * 5
-	var explosion_radius = ammo_storage/2
-	ammo_storage = 0
+	var max_explosive_damage = 100
+	var explosion_radius = explosion_shape.shape.radius
 	for body in explosion_area.get_overlapping_bodies():
 		if body.has_method("damage"):
 			var dist = global_position.distance_to(body.global_position)
